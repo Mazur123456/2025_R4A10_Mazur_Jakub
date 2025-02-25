9bf1139ab79efde856a5dc6417385cfe0f277492
Mazur123456 <jakubmazur1356@gmail.com>
2025-02-25 21:24:25 +0100

    add generate-page component with template, styles, and tests

---
+++ tp3/project/src/app/generate-page/generate-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generate-page',
+  imports: [],
+  templateUrl: './generate-page.component.html',
+  styleUrl: './generate-page.component.scss'
+})
+export class GeneratePageComponent {
+
+}
